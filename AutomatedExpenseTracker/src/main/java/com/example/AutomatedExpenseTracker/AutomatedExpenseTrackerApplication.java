6b124fa7bb1a70918247a4e082aed8d9139ba262
Micah <132845592+MibahM@users.noreply.github.com>
2024-06-14 20:23:19 -0500

    Add files via upload

---
+++ AutomatedExpenseTracker/src/main/java/com/example/AutomatedExpenseTracker/AutomatedExpenseTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.AutomatedExpenseTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedExpenseTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedExpenseTrackerApplication.class, args);
+	}
+
+}
