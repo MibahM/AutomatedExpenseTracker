c5ccff6d0cc8d59656872781b0ae7dc2a961db38
Micah <132845592+MibahM@users.noreply.github.com>
2024-06-14 20:32:13 -0500

    Add files via upload

---
+++ AutomatedExpenseTracker/src/test/java/com/example/AutomatedExpenseTracker/AutomatedExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AutomatedExpenseTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
